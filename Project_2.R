18e5b4194c574ac3d027858f296c5a0e70decc2f
Albert Jojo <jojoalbert35@gmail.com>
2025-10-27 23:17:33 -0400

    Fixed plotting feature, subsetting dataframe into sample_df, split plotting functions into sub_functions

--- Project_2.R
+++ Project_2.R
@@ -1,49 +1,72 @@
+# Libraries
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 
+# Data ingestion
 ingest_data <- function(path = "data/data.csv") {
     if (!file.exists(path)) {
         stop(paste("File not Found:", path))
     }
-    return(read.csv(path, header = TRUE, sep = ","))
+    read.csv(path, header = TRUE, sep = ",")
 }
+# Data processing
 process_data <- function(df) {
-    drop_cols <- c("Color", "Sales_Classification")
-    df <- df %>% select(-any_of(drop_cols))
-    return(df)
+    df <- df %>% select(-any_of(c("Color", "Sales_Classification")))
 }
-plot_data <- function(df) {
-    p <- ggplot(df, aes(x = Engine_Size_L, y = Sales_Volume, colour = model)) +
-        geom_point() +
+### Choosing subset for visualization
+create_subset <- function(df, size = 1000) {
+    sample_df <- df[sample(nrow(df), min(size, nrow(df))), ]
+}
+
+plot_histogram <- function(df, col, save_path = NULL) {
+    p <- ggplot(df, aes_string(x = col)) +
+        geom_histogram(fill = "blue", color = "black", alpha = 0.7) +
         labs(
-            title = "Engine_Size_L vs Sales_Volume",
-            x = "Engine Size (L)",
-            y = "Sales Volume"
+            title = paste("Histogram of ", col),
+            x = col,
+            y = "Count"
         ) +
         theme_minimal()
-    print(p)
+    if (!is.null(save_path)) ggsave(filename = save_path, plot = p, width = 8, height = 4)
+    return(p)
 }
-explore_data <- function(df) {}
+plot_box <- function(df, col, save_path = NULL) {
+    p <- ggplot(df, aes_string(y = col)) +
+        geom_boxplot(fill = "green", color = "black", alpha = 0.7) +
+        labs(title = paste("Box Plot of ", col), y = col) +
+        theme_minimal()
+    if (!is.null(save_path)) ggsave(filename = save_path, plot = p, width = 8, height = 4)
+    return(p)
+}
+plot_bar <- function(df, col, save_path = NULL) {}
+# Master function for plotting
+plot_features <- function(df) {
+    for (col in names(df)) {
+        if (is.numeric(df[[col]])) {
+            print(plot_histogram(df, col))
+            print(plot_box(df, col))
+        } else if (is.factor(df[[col]]) || is.character(df[[col]])) {}
+    }
+}
+
+# Save processed data
 write_data_to_csv <- function(df, output_path = "data/processed_data.csv") {
     write.csv(df, output_path, row.names = FALSE)
 }
 
 ## Driver function
-main <- function(run_processing = FALSE) {
+main <- function(run_processing = FALSE,
+                 raw_path = "data/data.csv",
+                 processed_path = "data/processed_data.csv",
+                 subset_size = 1000) {
     if (run_processing) {
-        message("Reading data ...")
-        df <- ingest_data(path = "data/data.csv")
-        message("Processing data ...")
-        df <- process_data(df)
-        message("Writing Processed data ...")
-        write_data_to_csv(df) # Write to CSV
+        df <- ingest_data(raw_path) %>% process_data()
+        write_data_to_csv(df, processed_path)
     } else {
-        message("Skipping processing, loading existing processing data...")
         df <- ingest_data("data/processed_data.csv")
-
-        message("\n Plotting Data")
-        plot_data(df)
+        sample_df <- create_subset(df)
+        plot_features(sample_df)
     }
 }
 if (sys.nframe() == 0) {
